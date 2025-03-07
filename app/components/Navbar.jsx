36615164571bb5b963072349f30d94aa46a92841
Shob3r <andrewpinter23@gmail.com>
2025-03-07 14:47:48 -0800

    navbar gaming

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,11 @@
 'use client'
 
-import { useState } from "react";
 export default function Navbar() {
     return(
-        <nav className="fixed bg-black border-b-1 border-white flex flex-auto w-screen h-15 text-center items-center p-5 z-50 shadow-2xl">
-            <button className="navbar-button">Projects</button>
-            <button className="navbar-button">About</button>
-            <button className="navbar-button">Articles</button>
-        </nav>
+    <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-black border-b border-l border-r rounded-b-2xl drop-shadow-xl shadow-md shadow-yellow-300/50 border-yellow-300 flex justify-center items-center h-15 p-2 z-50 w-2/5">
+        <button className="navbar-button">Projects</button>
+        <button className="navbar-button">About</button>
+        <button className="navbar-button">Articles</button>
+    </nav>
     )
 }
