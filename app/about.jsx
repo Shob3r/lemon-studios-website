3511550308114849f392854b09d760f236bd5d0c
Shob3r <andrewpinter23@gmail.com>
2025-03-07 13:22:46 -0800

    Work on navbar one day i'll be awesome at next :sunglasses:

---
+++ app/about.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function About() {
+    return(
+        <h1>temp</h1>
+    );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
         <Image
           className="dark m-5 justify-self-center"
           src="/logo.svg"
           alt="Next.js logo"
-          width={320}
-          height={320}
+          width={256}
+          height={256}
           priority />
         <p className="text-center text-2xl">Reconstruction (is probably) in progress...</p>
       </main>
