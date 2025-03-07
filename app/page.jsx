a075efac31700e15f02241e55cca440945fb09f7
Shob3r <andrewpinter23@gmail.com>
2025-03-07 09:52:06 -0800

    (first commit to the dev branch) centering issue fixed!!!!

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
         <Image
           className="dark m-5 justify-self-center"
           src="/logo.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={320}
+          height={320}
           priority />
-        <p>Reconstruction (is probably) in progress...</p>
-
+        <p className="text-center text-2xl">Reconstruction (is probably) in progress...</p>
       </main>
     </div>
   );
